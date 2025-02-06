06bd2c23fc46ed2063bae7dd2c577d24b6faecbe
Josh-KDG <joshkdg65@gmail.com>
2025-02-06 09:25:20 +0000

    chat2

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,9 +1,40 @@
 <div>
     {{-- Because she competes with no one, no one can compete with her. --}}
     <div class="chatlist_header">
+<div class="title">
+    Chat
+</div>
 
-    </div>
+<div class="img_container">
+    <img src="https://picsum.photos/id/237/200/300" alt="">
+</div>
+</div>
+
     <div class="chatlist_body">
+        <div class="chatlist_item">
+            <div class="chatlist_img_container">
+                <img src="https://picsum.photos/id/200/200/300" alt="">
+
+            </div>
+            <div class="chatlist_info">
+                <div class="top_row">
+                    <div class="list_username">Josh</div>
+
+                    <span class="date">2d</span>
+                </div>
+
+                <div class="bottom_row">
+
+                    <div class="message_body text-truncate">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae doloribus possimus nemo earum quasi quos, est reiciendis nostrum ipsam consequatur quas accusamus totam? Tempora tenetur eius quisquam, dolorem officia iure.
+                    </div>
+
+                    <div class="unread_count">
+                        56
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </div>
